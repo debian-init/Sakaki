90aa0ae343dfe5e624701c261920c5f189f5d699
debian-projects <debianprojetos@gmail.com>
2024-12-19 07:45:32 -0300

    Add files via upload

--- delete-commands.ts
+++ delete-commands.ts
@@ -33,7 +33,7 @@ const rest = new REST().setToken(token);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
+		const data = await rest.delete(
 			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
